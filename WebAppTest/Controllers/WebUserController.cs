597a25aab1fda82a6c0cb3322352df461e9c4848
bryce <bryceperez31@gmail.com>
2021-09-09 12:43:09 -0700

    Add project files.

---
+++ WebAppTest/Controllers/WebUserController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppTest.Models;
+
+namespace WebAppTest.Controllers
+{
+    public class WebUserController : Controller
+    {
+        // GET: WebUser
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(WebUserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("ThankYou", model);
+            }
+            return View(model);
+        }
+
+        public ActionResult ThankYou(WebUserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ WebAppTest/Models/WebUserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAppTest.Models
+{
+    public class WebUserModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string FullName { get => FirstName + " " + LastName; }
+
+    }
+}
